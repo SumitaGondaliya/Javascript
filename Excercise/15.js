2113d42afcf038a3c0f89e4fafca8c80db1d83c7
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-19 22:56:07 +0530

    filter_method

--- Excercise/15.js
+++ Excercise/15.js
@@ -13,8 +13,12 @@ You can expect valid strings for all test cases.
 */
 
 
-function maxWord(arr){
-    
+function maxWord(arr)
+{
+    return arr.filter((maxWord) => maxWord.length === 4)
+
 }
 
 console.log("This words has four letters",maxWord(["Tomato","Potato","Pair"]))
+console.log("This words has four letters",maxWord(["Kangaroo", "Bear", "Fox"]))
+console.log("This words has four letters",maxWord(["Ryan", "Kieran", "Jason", "Matt"]))
