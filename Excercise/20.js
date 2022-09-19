b66af7a79c3a81271fd41d303d1128a194fa0899
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-19 21:15:44 +0530

    concate_arr

---
+++ Excercise/20.js
@@ -0,0 +1,19 @@
+/*Concatenating Two Integer Arrays
+Create a function to concatenate two integer arrays.
+
+Examples
+concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+
+concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+
+concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+Notes
+Don't forget to return the result.
+*/
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+let add = arr1.concat(arr2);
+
+console.log(add);
