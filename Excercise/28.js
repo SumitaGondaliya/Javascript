dc8c85c7f8aa9b21f5217cd78b0de2e3dc665175
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-22 11:19:26 +0530

    even_odd_partion

---
+++ Excercise/28.js
@@ -0,0 +1,25 @@
+/*Even Odd Partition
+Write a function that partitions the array into two subarrays: one with all even integers, and the other with all odd integers. Return your result in the following format:
+
+[[evens], [odds]]
+Examples
+evenOddPartition([5, 8, 9, 2, 0]) ➞ [[8, 2, 0], [5, 9]]
+
+evenOddPartition([1, 0, 1, 0, 1, 0]) ➞ [[0, 0, 0], [1, 1, 1]]
+
+evenOddPartition([1, 3, 5, 7, 9]) ➞ [[], [1, 3, 5, 7, 9]]
+
+evenOddPartition([]) ➞ [[], []]
+Notes
+Return two empty subarrays if the input is an empty array.
+Keep the same relative ordering as the original array.*/
+
+function evenOddPartition(arr) {
+  var even = arr.filter((i) => i % 2 == 0);
+  var odd = arr.filter((i) => i % 2 == 1);
+  var result = even.concat(odd);
+
+  return result;
+}
+
+console.log(evenOddPartition([5, 8, 9, 2, 0]));
